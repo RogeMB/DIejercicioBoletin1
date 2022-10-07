34e4281c51acfe78f03cd192262809628bfbd7c3
RogeMB <mohigefer.barog22@triana.salesianos.edu>
2022-10-07 03:18:45 +0200

    Inicio card con el listado

--- pokemonService/src/app/components/pokemon-list/pokemon-list.component.ts
+++ pokemonService/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -11,11 +11,39 @@ export class PokemonListComponent implements OnInit {
 
   listadoPokemon: Pokemon[] = [];
 
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+
+
+
   constructor(private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
     this.pokemonService.pokemonList().subscribe(response => {
       this.listadoPokemon = response.results;
+      this.getPokemonsComp();
+      //this.pokemonService.getPokemon(this.getRandomInt())
     });
+  }
+
+  getRandomInt(){
+    var max= 151;
+    var min=1;
+    var num = Math.floor(Math.random() * (max - min)) + min;
+    return num;
+  }
+
+  getPokemonsComp(){
+    for (let index = 1; index <= 150; index++){
+      this.pokemonService.getPokemon(index).subscribe(
+        response=> {
+          
+        },
+        error=>{
+
+        }
+      )
+    }
   }
 }
--- pokemonService/src/app/services/pokemon.service.ts
+++ pokemonService/src/app/services/pokemon.service.ts
@@ -3,8 +3,8 @@ import {HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
 import {PokemonResponse} from '../interfaces/pokemon-response.interface'
 
-const API_BASE_URL = 'https://pokeapi.co/api/v2'
 
+const API_BASE_URL = 'https://pokeapi.co/api/v2'
 @Injectable({
   providedIn: 'root'
 })
@@ -12,13 +12,16 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
+
+
   public pokemonList(): Observable<PokemonResponse> {  //dato asíncrono, es decir, que se queda esperando al momento que llegue el dato para saber qué tipo es
     return this.http.get<PokemonResponse>(`${API_BASE_URL}/pokemon`);  //hay que especificarlo aquí también. Un json pasado a objeto primero y luego el observable entra en acción
     //return this.http.get(API_BASE_URL.concat("/pokemon"))
   }
 
-  public getPokemon(id:string){
-    return this.http.get(`${API_BASE_URL}/id`);
+  public getPokemon(index:any){
+    return this.http.get(`${API_BASE_URL}/${index}`);
      //return this.http.get(API_BASE_URL.concat("/id"))
+     // return this.http.get(`${API_BASE_URL}/id`); esta es la original con el id:string de parametro
   }
 }
